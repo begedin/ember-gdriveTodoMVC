0a94492d6ee86cef4f8b083b50965942dd142a87
begedin <begedinnikola@gmail.com>
2015-01-12 20:21:05 +0100

    Reworked logic related to logging in. Now part of ember-gdrive
    * We now use a `LoginControllerMixin` from inside the `login` controller.
    * No need to use `login` route for anything
    * No need to use `index` route for anything
    * `ApplicationRouteMixin` now holds all logic from `application` and `index` routes.
    * `login` template now uses `authenticate` action instead of `login`

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-gdrive/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
-  actions: {
-    logout: function () {
-      var route = this,
-        session = this.get('session');
-
-      session.invalidate().then(function () {
-        route.transitionTo('login');
-      });
-    }
-  }
-});
+export default Ember.Route.extend(ApplicationRouteMixin, {});
