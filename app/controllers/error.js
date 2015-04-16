21abf92fb1f18321cca30ba5e32a5dfb89193394
begedin <begedinnikola@gmail.com>
2015-04-16 11:42:50 +0200

    Switched to regular from ObjectController-s

--- app/controllers/error.js
+++ app/controllers/error.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 var NOT_FOUND_REGEX = /The file was not found\. It does not exist or you do not have read access to the file\./;
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
 
   isAccessError: function(){
-    var message = this.get('message');
+    var message = this.get('model.message');
     return (NOT_FOUND_REGEX.test(message));
   }.property('message')
 
